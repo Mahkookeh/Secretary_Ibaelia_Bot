6bb45ec6b68b09da7ac2bf2b49c801ed25692b78
Mahkookeh <kraw92606@gmail.com>
2020-06-25 13:55:05 -0700

    Added source code link

--- cogs/ibaelia.py
+++ cogs/ibaelia.py
@@ -7,9 +7,17 @@ class IbaeliaCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="sourcecode", help="Don't stare too long!")
+    async def sourcecode(self, ctx):
+        embed = discord.Embed(title="You want to look at my *what*?! :flushed:",color=0x14e1d4)
+        embed.add_field(value="Oh gosh, don't stare too long!\n[(⁄⁄•⁄ω⁄•⁄⁄)](https://github.com/Mahkookeh/Secretary_Ibaelia_Bot)\nHow embarrassing...", name='\u200b')
+        # embed.add_field(value="[(⁄⁄•⁄ω⁄•⁄⁄)](https://github.com/Mahkookeh/Secretary_Ibaelia_Bot)", name='\u200b')
+        # embed.add_field(value="How embarrassing...", name='\u200b')
+        embed.set_footer(text="uwu")
+        await ctx.send(embed=embed)
+
     @commands.command(name="unsubscribe", help="Unsubscribe from my antics.")
     async def unsubscribe(self, ctx):
-        emoji = get(ctx.message.guild.emojis, name="cry")
         embed = discord.Embed(title="You meanie! :cry:", description=f"You're stuck with me though...",color=0x14e1d4)
         irelia_buns = "ibaelia_images//sad_irelia.png"
         file = discord.File(irelia_buns, filename="image.png")
